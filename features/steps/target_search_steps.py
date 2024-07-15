2592bd5baaace5359993369f956cb1183a47dfa9
Arshdeep2933 <arshdhillon1699@gmail.com>
2024-07-15 13:34:33 -0700

    updated

--- features/steps/target_search_steps.py
+++ features/steps/target_search_steps.py
@@ -10,14 +10,13 @@ def open_target(context):
     context.driver.get('https://www.target.com/')
 
 
-@when('Search for Product')
-def search_product(context):
-    context.driver.find_element(By.ID, 'search').send_keys('tea')
+@when('Search for {product}')
+def search_product(context, product):
+    context.driver.find_element(By.ID, 'search').send_keys(product)
     context.driver.find_element(By.CSS_SELECTOR, "[class='sc-9d95d074-3 kYwmqK']").click()
     sleep(6)
 
-@then('Verify search worked')
-def verify_search_results(context):
-    expected_text = 'tea'
+@then('Verify search results shown for {product}')
+def verify_search_results(context, product):
     actual_text = context.driver.find_element(By.XPATH, "//div[@data-test='resultsHeading']").text
-    assert expected_text in actual_text, f'Expected {expected_text} ot in actual {actual_text}'
+    assert product in actual_text, f'Expected {product} not in actual {actual_text}'
--- features/tests/target_search.feature
+++ features/tests/target_search.feature
@@ -3,5 +3,5 @@ Feature: Target main page search tests
 
   Scenario: user can search for a product on target
     Given Open target main page
-    When Search for Product
-    Then Verify search worked
+    When Search for coffee
+    Then Verify search results shown for coffee
